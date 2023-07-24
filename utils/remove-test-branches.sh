8f044080923748f4504e3373fbd7599349ab2863
JC <jchan48@gmail.com>
2023-07-24 15:52:43 -0400

    updated remove-test-branches.sh

--- utils/remove-test-branches.sh
+++ utils/remove-test-branches.sh
@@ -4,18 +4,13 @@
 # pipe to file
 # git ls-remote --exit-code --heads origin #
 git ls-remote --exit-code --heads origin | tee .cache/git-branch-remote.txt
-# grep .cache/git-branch-remote.txt -e "heads/rnggg" || echo 'not exists' #
-# grep .cache/git-branch-remote.txt -e "heads/main" && echo 'exists' #
+# grep "heads/main" .cache/git-branch-remote.txt && echo 'exists' #
+# grep "heads/main-sdfsdfsdf" .cache/git-branch-remote.txt || echo 'dne' #
 
 ITEMS=(a b ab)
 for ITEM in ${ITEMS[@]}; do
     echo $ITEM # a b ab
-    # todo maybe check
-
     git branch -D $ITEM
-
     # delete if exists
-    grep .cache/git-branch-remote.txt -e "heads/$ITEM" && git push origin --delete $ITEM
-    
-    
+    grep "heads/$ITEM" .cache/git-branch-remote.txt && git push origin --delete $ITEM
 done
